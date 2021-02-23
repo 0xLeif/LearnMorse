07473f357f31f57aa23b7f0f9b6f226ec3595a07
Zach Eriksen <zmeriksen@icloud.com>
2021-02-22 18:05:24 -0600

    Initial Commit

---
+++ Shared/LearnMorseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMorseApp.swift
+//  Shared
+//
+//  Created by Leif on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnMorseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
